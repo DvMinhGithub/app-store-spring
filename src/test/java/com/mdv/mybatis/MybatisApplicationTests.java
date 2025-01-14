04fe7abd34b8e0fd2c8ec5204a8f801c10089aa7
Minh Dinh <minhdv.dev@gmail.com>
2025-01-14 11:51:09 +0700

    chore: Initialize Spring Boot project with MyBatis integration

---
+++ src/test/java/com/mdv/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdv.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
