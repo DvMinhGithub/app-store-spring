304d308a718c3841cf77b2f48f0053b1ca863fc5
Minh Dinh <minhdv.dev@gmail.com>
2025-01-20 12:48:57 +0700

    feat: Add Category management functionality

---
+++ src/main/java/com/mdv/mybatis/model/request/CategoryRequest.java
@@ -0,0 +1,14 @@
+package com.mdv.mybatis.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryRequest {
+    private String name;
+}
