512fcd49a05a24e646abc3bdc38a47a4361e6eaa
Minh Dinh <minhdv.dev@gmail.com>
2025-01-22 11:16:22 +0700

    feat: Add factory methods to ApiResponse for success responses

--- src/main/java/com/mdv/mybatis/model/response/ApiResponse.java
+++ src/main/java/com/mdv/mybatis/model/response/ApiResponse.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
@@ -19,4 +20,17 @@ public class ApiResponse<T> {
     int code;
     String message;
     T data;
+
+    // Factory method
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(HttpStatus.OK.value(), message, data);
+    }
+
+    public static ApiResponse<Void> success(String message) {
+        return new ApiResponse<>(HttpStatus.OK.value(), message, null);
+    }
+
+    public static <T> ApiResponse<T> of(int code, String message, T data) {
+        return new ApiResponse<>(code, message, data);
+    }
 }
